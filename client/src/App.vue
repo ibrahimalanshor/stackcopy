b16b9dd522a80f685aeec67b896b1aca0b04a7fa
Ibrahim Al Anshor <ibrahimalanshor6@gmail.com>
2021-05-08 13:38:45 +0700

    Add Navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <navbar />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navbar
   }
 }
 </script>
---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,57 @@
+<template>
+  <nav class="navbar">
+    <div class="container">
+      <div class="row navbar-row">
+        <ul class="col navbar-nav">
+          <li><a href="" class="col navbar-link active">Question</a></li>
+          <li><a href="" class="col navbar-link">Post</a></li>
+          <li><a href="" class="col navbar-link">Jobs</a></li>
+        </ul>
+        <div class="col navbar-toggle">
+          <button>
+            <i class="fa fa-bars"></i>
+          </button>
+        </div>
+        <form action="" class="col navbar-form">
+          <input type="search" class="navbar-search input" placeholder="Search">
+        </form>
+        <ul class="row col">
+          <li class="col"><a href="" class="button white">Login</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style>
+  .navbar {
+    @apply h-16 bg-gray-800 text-white flex items-center;
+  }
+  .navbar-row {
+    @apply items-center;
+  }
+  .navbar-nav {
+    @apply items-center hidden -mx-2;
+  }
+  .navbar-link {
+    @apply text-gray-400;
+  }
+  .navbar-link:hover, .navbar-link.active {
+    @apply text-gray-100;
+  }
+  .navbar-form {
+    @apply flex-grow;
+  }
+  .navbar-search {
+    @apply text-gray-800;
+  }
+
+  @screen sm {
+    .navbar-nav {
+      @apply flex;
+    }
+    .navbar-toggle {
+      @apply hidden;
+    }
+  }
+</style>
