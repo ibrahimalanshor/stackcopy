d25fd3b427b5750c3b300f172c54fd0366a42d0c
Ibrahim Al Anshor <ibrahimalanshor6@gmail.com>
2021-05-29 08:31:02 +0700

    Fix Login Request

--- server/src/requests/auth/login.js
+++ server/src/requests/auth/login.js
@@ -4,7 +4,7 @@ const { userExists } = require('./validator')
 
 const rules = [
   body('email').exists().isEmail().custom(userExists),
-  body('password').exists().isString()
+  body('password').exists().isString().isLength({ min: 1})
 ]
 
 module.exports = [
